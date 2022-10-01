e464a48a32619831f933b8c501670b88ca6908a9
Rodriques <rodriques.perry@gmail.com>
2022-09-30 23:50:23 -0500

    update: add react-helmet to contact page

--- frontend/src/routes/contact/contact.component.jsx
+++ frontend/src/routes/contact/contact.component.jsx
@@ -1,8 +1,18 @@
+import { Helmet } from 'react-helmet';
+
 import ContactPageForm from '../../components/contact-forms/contact-page-contact-form.compnent';
 
 const Contact = () => {
   return (
     <div>
+      <Helmet>
+        <meta
+          name='description'
+          content='Contact us for web design services'
+        />
+        <meta name='keywords' content='web design seo contact' />
+        <title>Contact Us</title>
+      </Helmet>
       <ContactPageForm />
     </div>
   );
