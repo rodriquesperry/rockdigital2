44086e73c772ce06914948280b676944a4262e4d
Rodriques <rodriques.perry@gmail.com>
2022-10-04 21:38:56 -0500

    update: Helmet added to blog

--- frontend/src/routes/blog/blog.component.jsx
+++ frontend/src/routes/blog/blog.component.jsx
@@ -1,41 +1,28 @@
-import { useEffect, useState } from 'react';
-
 import { Helmet } from 'react-helmet';
 
-import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 import FeaturedPost from '../../components/featured-blog-post/FeaturedPost.component';
 import BlogPost from '../../components/blog-post/BlogPost.component';
 
-import axios from 'axios';
-
 import './blog.styles.css';
 
 const Blog = () => {
-  const [error, setError] = useState(null);
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const getPosts = async () => {
-      await axios
-        .get('http://localhost:1337/api/posts?populate=*')
-        .then((data) => setPosts(data.data))
-        .catch((error) => setError(error));
-    };
-    getPosts();
-  }, []);
-
-  
   return (
     <div>
+      <Helmet>
+        <meta
+          name='description'
+          content='A blog about web design, search engine optimization, website maintenance and website improvement for business owners'
+        />
+        <meta name='keywords' content='web design seo' />
+        <title>Blog</title>
+      </Helmet>
       <div className='img-container'>
         <h1 id='text'>THE BLOG</h1>
       </div>
       <Row className='posts-container'>
         <FeaturedPost />
-        
       </Row>
       <Row>
         <BlogPost />
