43bb6ab7044ce8906d837f609bf745efd4c29e5c
Rodriques <rodriques.perry@gmail.com>
2022-10-04 21:34:24 -0500

    update: featuredPost component

---
+++ frontend/src/components/featured-blog-post/FeaturedPost.component.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
+const FeaturedPost = () => {
+  const [error, setError] = useState(null);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      await axios
+        .get('http://localhost:1337/api/posts?populate=*')
+        .then((data) => setPosts(data.data))
+        .catch((error) => setError(error));
+    };
+    getPosts();
+  }, []);
+
+  if (error) {
+    return <div>An error occurred: {error.message}</div>;
+  }
+
+  return (
+    <>
+      {posts.data &&
+        posts.data.map((post, i) => {
+          if (post.attributes.featured) {
+            return (
+              <Col xs={(6, { order: 1 })} className='featured-container'>
+                <Card key={i} className='featured'>
+                  <Card.Img
+                    variant='top'
+                    src={`http://localhost:1337${post.attributes.Image.data.attributes.url}`}
+                  />
+                  <Card.Body>
+                    <Card.Title>{post.attributes.Title}</Card.Title>
+                    <Card.Text>{post.attributes.short_description}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          }
+        })}
+    </>
+  );
+};
+
+export default FeaturedPost;
--- frontend/src/routes/blog/blog.component.jsx
+++ frontend/src/routes/blog/blog.component.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 
+import { Helmet } from 'react-helmet';
+
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import FeaturedPost from '../../components/featured-blog-post/FeaturedPost.component';
 import BlogPost from '../../components/blog-post/BlogPost.component';
 
 import axios from 'axios';
@@ -13,8 +16,6 @@ import './blog.styles.css';
 const Blog = () => {
   const [error, setError] = useState(null);
   const [posts, setPosts] = useState([]);
-  const [featured, setFeatured] = useState({});
-  const featuredContent = [];
 
   useEffect(() => {
     const getPosts = async () => {
@@ -26,34 +27,14 @@ const Blog = () => {
     getPosts();
   }, []);
 
-  if (error) {
-    return <div>An error occurred: {error.message}</div>;
-  }
+  
   return (
     <div>
       <div className='img-container'>
         <h1 id='text'>THE BLOG</h1>
       </div>
       <Row className='posts-container'>
-        {posts.data &&
-          posts.data.map((post, i) => {
-            if (post.attributes.featured) {
-              return (
-                <Col xs={(6, { order: 1 })} className='featured-container'>
-                  <Card key={i} className='featured'>
-                    <Card.Img
-                      variant='top'
-                      src={`http://localhost:1337${post.attributes.Image.data.attributes.url}`}
-                    />
-                    <Card.Body>
-                      <Card.Title>{post.attributes.Title}</Card.Title>
-                      <Card.Text>{post.attributes.short_description}</Card.Text>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              );
-            }
-          })}
+        <FeaturedPost />
         <Col xs={3} lg={{ order: 2 }}>
           {posts.data &&
             posts.data.map((post, i) => {
