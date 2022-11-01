ae5a3e58fb89c0cd0def3e306c5cebd617fe3de3
Rodriques <rodriques.perry@gmail.com>
2022-11-01 12:32:24 -0500

    update: reviews

--- frontend/src/components/carousels/reviews/carousel-review.component.jsx
+++ frontend/src/components/carousels/reviews/carousel-review.component.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 import Carousel from 'react-bootstrap/Carousel';
 
@@ -8,52 +10,50 @@ import me from '../../../assets/IMG-1561 copy 2.png';
 import './carousel-review.styles.css';
 
 const CarouselReview = () => {
+  const [error, setError] = useState(null);
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const getReviews = async () => {
+      await axios
+        .get('https://rockdigital.agency/dashboard/api/reviews?populate=*')
+        .then((data) => setReviews(data.data.data));
+    };
+    getReviews();
+  }, []);
+
+  console.log(reviews);
+
   return (
     <div className='review-carousel-container'>
       <Carousel interval={5000} variant='dark' fade>
         <Carousel.Item>
           <div className='review row'>
-            <div className='col-12 col-lg-3 image-container'>
-              <img src={me} alt='' className='img-fluid review-image' />
-            </div>
-            <div className='col review-container'>
-              <p>
-                <span className='quote'>"</span>Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Optio quos animi, eaque aperiam
-                saepe dolores consequatur recusandae dolorum.
-                <span className='quote'>"</span>
-              </p>
-            </div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className='review row'>
-            <div className='col-12 col-lg-3 image-container'>
-              <img src={me} alt='' className='img-fluid review-image' />
-            </div>
-            <div className='col review-container'>
-              <p>
-                <span className='quote'>"</span>Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Optio quos animi, eaque aperiam
-                saepe dolores consequatur recusandae dolorum.
-                <span className='quote'>"</span>
-              </p>
-            </div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className='review row'>
-            <div className='col-12 col-lg-3 image-container'>
-              <img src={me} alt='' className='img-fluid review-image' />
-            </div>
-            <div className='col review-container'>
-              <p>
-                <span className='quote'>"</span>Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Optio quos animi, eaque aperiam
-                saepe dolores consequatur recusandae dolorum.
-                <span className='quote'>"</span>
-              </p>
-            </div>
+            {reviews &&
+              reviews.map((review, i) => {
+                return (
+                  <>
+                    {/* <div className='col-12 col-lg-3 image-container'>
+                      <img
+                        src={`https://rockdigital.agency/dashboard${review.attributes.review_image.data.attributes.url}`}
+                        alt=''
+                        className='img-fluid review-image'
+                      />
+                </div> */}
+                    <div className='col review-container'>
+                      <p>
+                        <span className='quote'>"</span>
+                        {review.attributes.review_quote}
+                        <span className='quote'>"</span>
+                      </p>
+                    </div>
+                    <div className="reviewer-container">
+                    <p>~</p>
+                    <h4 className='reviewer'>{review.attributes.reviewer_name}</h4>
+                    </div>
+                  </>
+                );
+              })}
           </div>
         </Carousel.Item>
       </Carousel>
