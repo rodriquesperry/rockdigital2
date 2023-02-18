523134614bfbeb36871d683311f9c1cf9147748e
Rodriques <rodriques.perry@gmail.com>
2023-02-17 23:29:37 -0600

    Add responsive image sizes to carousel to fix LCP performance

--- frontend/src/components/carousels/top-page/carousel.component.jsx
+++ frontend/src/components/carousels/top-page/carousel.component.jsx
@@ -8,18 +8,7 @@ import WebDesign1000w from '../../../assets/web-design-full_1000px.jpeg';
 import WebDesign1359w from '../../../assets/web-design-full_1359px.jpeg';
 import WebDesign2479w from '../../../assets/web-design-full_2479px.jpeg';
 
-import DigitalMarketing344w from '../../../assets/digital-marketing_344px.jpeg';
-import DigitalMarketing687w from '../../../assets/digital-marketing_687px.jpeg';
-import DigitalMarketing1000w from '../../../assets/digital-marketing_1000px.jpeg';
-import DigitalMarketing1359w from '../../../assets/digital-marketing_1359px.jpeg';
-import DigitalMarketing2479w from '../../../assets/digital-marketing_2479px.jpeg';
 
-
-import SheratonHotel344w from '../../../assets/sheraton-houston-st-san-antonio-tx_344px.jpeg';
-import SheratonHotel687w from '../../../assets/sheraton-houston-st-san-antonio-tx_687px.jpeg';
-import SheratonHotel1000w from '../../../assets/sheraton-houston-st-san-antonio-tx_1000px.jpeg';
-import SheratonHotel1359w from '../../../assets/sheraton-houston-st-san-antonio-tx_1359px.jpeg';
-import SheratonHotel2479w from '../../../assets/sheraton-houston-st-san-antonio-tx_2479px.jpeg';
 
 // import '../../../scss/carousel.styles.scss';
 import './carousel.styles.css';
